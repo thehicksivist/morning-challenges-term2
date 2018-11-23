b4b5c612c9bb8cf7e66d908e1104d16c5ce804a7
thehicksivist <hicks.a.james@gmail.com>
2018-11-23 11:41:45 +1000

    05 completed

--- 05_sum_numbers.js
+++ 05_sum_numbers.js
@@ -3,8 +3,25 @@
 // Test your solution:
 // mocha 05_sum_numbers.js
 
+function addScores(scores) {
+    const add = (a, b) => a + b
+    const sum = scores.reduce(add)
+    return sum
+}
+
 function findWinner(players) {
     // Your code here
+    
+    for (player of players) {
+        let total = addScores(player.scores)
+        player.scores = total
+    }
+   
+    if (players[0].scores >= players[1].scores) {
+        return players[0].name
+    }   else {
+        return players[1].name
+    }
 
 }
 
@@ -26,7 +43,7 @@ describe('findWinner', function () {
     })
     it('Should return the winner when winner is second in array', function () {
         players = [new Player('Nathan', [55, 90, 86]), new Player('James', [200, 100, 85])]
-        assert.equal('Nathan', findWinner(players))
+        assert.equal('James', findWinner(players))
     })
     it('Should return the first player when both players are tied', function () {
         players = [new Player('Nathan', [50, 100, 85]), new Player('James', [50, 100, 85])]
